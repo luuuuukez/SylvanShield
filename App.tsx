af6bffc8115c613455051a078568afb7040c734e
luuuuukez <lukezzzhw@gmail.com>
2026-03-05 12:01:15 +0200

    feat: initial project setup and nativewind configuration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
